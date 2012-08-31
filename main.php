2c1fb0fe1a1c2e786eb71eb23e6b40e5a10688d9
Matt Sanford <pucksmasher@verizon.net>
2012-08-31 09:43:37 -0400

    changes

--- main.php
+++ main.php
@@ -39,7 +39,8 @@
 			</script>
 		</div>
 	</div>
-	<div id="sidebar">
+	
+	<div class="sidebar">
 		<img class="pl" src="" />
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pretium dolor et neque tincidunt dictum. Phasellus tristique varius dignissim. Duis ut aliquam neque. Pellentesque molestie quam eget sem dignissim eleifend. Aliquam id massa sed ligula tempus pellentesque eu eget magna. Nulla sodales nisi eu mi molestie sit amet vehicula urna fringilla. Integer at neque sed arcu elementum fringilla at eget sapien. Nullam arcu augue, fringilla non tempus quis, dapibus non ligula.</p>
 	</div>
