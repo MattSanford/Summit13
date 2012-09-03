52dde48f466fb7cf675dbcf7dced84694815b457
Matt Sanford <pucksmasher@verizon.net>
2012-09-03 06:55:11 -0400

    updates

--- location.php
+++ location.php
@@ -1,6 +1,12 @@
 <?php include("header.php"); ?>
 <?php include("main.php"); ?>
 <div id="content">
+<iframe width="880" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=+401+West+Pratt+Street,+Baltimore,+MD&amp;aq=&amp;sll=39.286084,-76.621399&amp;sspn=0.084369,0.165825&amp;ie=UTF8&amp;hq=&amp;hnear=401+W+Pratt+St,+Baltimore,+Maryland+21201&amp;t=m&amp;ll=39.291797,-76.617365&amp;spn=0.023249,0.036478&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=+401+West+Pratt+Street,+Baltimore,+MD&amp;aq=&amp;sll=39.286084,-76.621399&amp;sspn=0.084369,0.165825&amp;ie=UTF8&amp;hq=&amp;hnear=401+W+Pratt+St,+Baltimore,+Maryland+21201&amp;t=m&amp;ll=39.291797,-76.617365&amp;spn=0.023249,0.036478&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>	
+</div>
+<div class="bottombar">
+		<img class="pl" src="" />
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pretium dolor et neque tincidunt dictum. Phasellus tristique varius dignissim. Duis ut aliquam neque. Pellentesque molestie quam eget sem dignissim eleifend. Aliquam id massa sed ligula tempus pellentesque eu eget magna. Nulla sodales nisi eu mi molestie sit amet vehicula urna fringilla. Integer at neque sed arcu elementum fringilla at eget sapien. Nullam arcu augue, fringilla non tempus quis, dapibus non ligula.</p>
+</div>
 </div>
 </div>
 <?php include("footer.php"); ?>
