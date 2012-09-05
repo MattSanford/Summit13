b774f55eae7ea393f67185683266d79ac2096fb9
Matt Sanford <pucksmasher@verizon.net>
2012-09-05 16:11:06 -0400

    leaders

---
+++ leaders.php
@@ -0,0 +1,106 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/summit.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href='http://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div id="container">
+<span id="imagine">imagine</span>
+<header>
+
+	<ul id="top-nav">
+		<a href="index.php"><li>Home</li></a>
+		<a href="location.php"><li>Location</li></a>
+		<a href="cost.php"><li>Cost</li></a>
+		<a href="registration.php"><li>Registration</li></a>
+		<a href="transportation.php"><li>Transportation</li></a>
+		<a href="schedule.php"><li>Schedule</li></a>
+		<a href="things-to-do.php"><li>Things To Do</li></a>
+	</ul>
+	
+</header>
+
+<div id="wrapper">
+		<ul id="checkout">
+			<li>
+			<ul id="checkout-nav">
+				<a href="http://www.facebook.com/FoursquareSummitEast"target="blank"><li id="facebook">
+					<img class="icon" src="images/f_logo.png" />
+					<span>Facebook</span>
+					<p>Check out our page!</p>
+				</li></a>
+				<a href="http://twitter.com/SummitEast13" target="blank"><li id="twitter">
+					<img class="icon" src="images/twitter-bird-white-on-blue.png" />				
+					<span>Twitter</span>
+					<p>Follow Us on twitter!</p>
+				</li></a>
+			</ul>
+			</li>
+		</ul>
+		<div id="sidebar-left">
+		<div>
+<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'profile',
+  rpp: 4,
+  interval: 30000,
+  width: 'auto',
+  height: 300,
+  theme: {
+    shell: {
+      background: '#003366',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#000000',
+      color: '#ffffff',
+      links: '#cc6600'
+    }
+  },
+  features: {
+    scrollbar: false,
+    loop: false,
+    live: false,
+    behavior: 'all'
+  }
+}).render().setUser('SummitEast13').start();
+</script>
+		</div>
+	</div>
+<div id="main">
+	<div id="social-bar">
+<!-- Place this tag where you want the +1 button to render. -->
+		<div class="g-plusone" data-size="medium"></div>
+		<a href="https://twitter.com/share" class="twitter-share-button" data-text="I am going to" data-hashtags="FoursquareSummitEast">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		<div class="fb-like fb" data-href="http://www.facebook.com/FoursquareSummitEast" data-send="false" data-width="450" data-show-faces="false"></div>
+	</div>
+
+	<div id="content">
+	<h1>This Site is for leaders only</h1>
+	</div>
+	<div class="bottombar">
+		<img class="pl" src="" /><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pretium dolor et neque tincidunt dictum. Phasellus tristique varius dignissim. Duis ut aliquam neque. Pellentesque molestie quam eget sem dignissim eleifend. Aliquam id massa sed ligula tempus pellentesque eu eget magna. Nulla sodales nisi eu mi molestie sit amet vehicula urna fringilla. Integer at neque sed arcu elementum fringilla at eget sapien. Nullam arcu augue, fringilla non tempus quis, dapibus non ligula.</p>
+</div>
+</div>
+</div>
+<div id="footer">
+<span class="Scripture"> blah blah blah</span>
+</div>
+
+</div>
+</body>
+</html>
