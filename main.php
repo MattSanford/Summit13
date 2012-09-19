83431b9f51efe3fa059932a08f187eb467c6e44c
Matt Sanford <pucksmasher@verizon.net>
2012-09-18 22:16:39 -0400

    changes

--- main.php
+++ main.php
@@ -2,6 +2,9 @@
 	<ul id="checkout">
 				<li>
 				<ul id="checkout-nav">
+					<li id="countdown">
+<iframe class="countdown" src="http://free.timeanddate.com/countdown/i39wpchv/n419/cf12/cm0/cu4/ct0/cs0/ca0/co1/cr0/ss0/cacf90/cpc000/pct/tcfff/fs100/szw320/szh135/tatCountdown%20to%20Summit/tac000/tptTime%20since%20Event%20started%20in/tpc000/iso2013-04-13T00:00:00/pa5" frameborder="0" width="207" height="59"></iframe>
+					</li>				
 					<a href="http://www.facebook.com/FoursquareSummitEast"target="blank"><li id="facebook">
 						<img class="icon" src="images/f_logo.png" />
 						<span>Facebook</span>
@@ -12,6 +15,7 @@
 						<span>Twitter</span>
 							<p>Follow Us on Twitter!</p>
 					</li></a>
+
 				</ul>
 				</li>
 	</ul>
