6f3e8fb8cb50ed9feb889be30cec20b78e3f518b
Matt Sanford <pucksmasher@verizon.net>
2012-09-18 22:19:59 -0400

    changes

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-<span class="Scripture"> blah blah blah</span>
+<span class="Scripture">"No eye has seen, No ear has heard, No mind can IMAGINE what God has planned for those who love Him." 1 Corinthians 2:9</span>
 </div>
 </div>
 </body>
