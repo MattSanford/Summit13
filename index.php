bc456130f1e1288bf4f8161992e7561626ed310e
Matt Sanford <pucksmasher@verizon.net>
2012-09-18 22:23:40 -0400

    changes

--- index.php
+++ index.php
@@ -115,7 +115,7 @@ new TWTR.Widget({
 </div>
 </div>
 <div id="footer">
-<span class="Scripture"> blah blah blah</span>
+<span class="Scripture">"No eye has seen, No ear has heard, No mind can IMAGINE what God has planned for those who love Him." 1 Corinthians 2:9</span>
 </div>
 
 </div>
