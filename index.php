3bd94b97c5fdda0391ba7905fd26db37d5be87db
Matt Sanford <pucksmasher@verizon.net>
2012-09-21 14:41:09 -0400

    changes

--- index.php
+++ index.php
@@ -17,8 +17,6 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 <div id="container">
-	<div id="imagine-wrap">
-	</div>
 <header>
 
 	<ul id="top-nav">
@@ -92,10 +90,10 @@ new TWTR.Widget({
 	</div>
 	
 		<div  class="featuring-section cj">
-			<img src="" class="pic right" />
-
-			<span>Special Guest Speaker:<br /><br/><span class="cj-span">Cornell Jordan</span></span>
-		</div></a>	
+			<img src="images/CJM_Promo_Pictures/cornelljordan-promopix4.jpg" class="pic right" />
+			<h2>Cornell Jordan</h2>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>		
+</div>
 		</div>
 <div id="main">
 	<div id="social-bar">
