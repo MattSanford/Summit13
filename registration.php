ed4be91ef6e2c0a170e2fce86e0d6ad6d6083a85
Matt Sanford <pucksmasher@verizon.net>
2012-12-10 16:25:06 -0500

    PRAY THIS LINK WILL WORK

--- registration.php
+++ registration.php
@@ -2,12 +2,12 @@
 <?php include("header.php"); ?>
 <?php include("main.php"); ?>
 	<div id="content">
-	<span class="bbspan">Online Registration:</span><span> <a href="www.acteva.com/go/midatlantic" target="blank">Click here!</a>  Choose "individual registration" and then the of quantity registrations you are ordering from the list provided.  Add the names of those you are registering at the bottom of the registration form. </span>
+	<span class="bbspan">Online Registration:</span><span> <a href="http://www.acteva.com/booking.cfm?bevaID=233347&CFID=8647005&CFTOKEN=5b10ac280cb30e84-428A109F-3048-344A-3CF13D5FB89DC79E" target="blank">Click here!</a>  Choose "individual registration" and then the of quantity registrations you are ordering from the list provided.  Add the names of those you are registering at the bottom of the registration form. </span>
 	<h3>Registration: Per Person</h3>
 	<h5>(Includes Saturday Lunch, late night activities and snacks and all summit materials)</h5>
 	<ul>
 		<li><span>$75 (Through January 15<sup>th</sup>)<span></li>
-		<li><span>$80 (Through March 31<sup>st</sup>)</span></li>
+		<li><span>$85 (Through March 31<sup>st</sup>)</span></li>
 	</ul>			
 	<p class="download"><a href="Summit Student and Leader Registrationpdf.pdf.zip">Download PDF Student &amp Leader Registration Form Here</a></p>
 
