418b5f4e190fcb4ef8b7bc5c91acee505df1ee30
Matt Sanford <pucksmasher@verizon.net>
2013-02-11 17:31:21 -0500

    how  bout now massaahh'

---
+++ Video-Contest/index.php
@@ -0,0 +1,16 @@
+<?php $pageTitle="<title>Video Contest for Imagine Summit '13</title>"?>
+<?php include ('../header.php'); ?>
+<?php include ('../main.php'); ?>
+<div class="content" id="content">
+	<img id="summit-talent" src="/images/Video Contest Promo.jpg">
+	<h1>Imagine Youth Summit Video Promo Contest</h1>
+	<h3>A video contest to find which youth group is the most excited for the upcoming Imagine Youth Summit!</h3>
+	<h2>Video Contest Guidelines:</h2>
+	<h4>1) Video must be less than 150 seconds (2 minutes and 30 seconds)</h4>
+	<h4>2) Points given for creativity/number of students/leaders in the video</h4>
+	<h4>3) Video link must be emailed to <a href="mailto:lklingenmeier@foursquare.org?Subject=Video%20Contest">lklingenmeier@foursquare.org</a> by March 15.</h4>
+	<h1 class="box-heading">Winning youth group will receive a FREE PIZZA PARTY on Friday Night!</h1>
+</div>
+</div>
+</div>
+<?php include ('../footer.php'); ?>
